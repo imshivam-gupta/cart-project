b329160ef545235d446b1f6f2661baea3e710124
imshivam-gupta <gupta.shivam2604@gmail.com>
2022-12-28 19:32:55 +0530

    Added events in cart-item

--- src/CartItem.js
+++ src/CartItem.js
@@ -8,7 +8,34 @@ class CartItem extends React.Component{
             qty: 1,
             img:' '
         }
+        this.testing();
     }
+    
+        testing (){
+            const promise = new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    resolve('done');
+                },5000);
+            })
+
+            promise.then(()=>{
+                this.setState({qty:100});
+                console.log('this.state',this.state);
+            });
+        }
+    
+
+        increaseQty = () =>{
+
+            // this.setState({qty:this.state.qty+1});
+        }
+
+    decreaseQty = () =>{
+        const {qty}=this.state;
+        if(qty==0) return;
+        this.setState({qty:this.state.qty-1});
+    }
+    
     render(){
         const {price,title,qty,img} = this.state;
         return(
@@ -25,13 +52,15 @@ class CartItem extends React.Component{
                         <img 
                             alt="increase" 
                             className='action-icons' 
-                            src='https://cdn-icons-png.flaticon.com/512/992/992651.png' 
+                            src='https://cdn-icons-png.flaticon.com/512/992/992651.png'
+                            onClick={this.increaseQty}
                         />
 
                         <img 
                             alt="decrease" 
                             className='action-icons' 
-                            src='https://cdn-icons-png.flaticon.com/512/992/992683.png' 
+                            src='https://cdn-icons-png.flaticon.com/512/992/992683.png'
+                            onClick ={this.decreaseQty}
                         />
 
                         <img 
